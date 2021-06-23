af038bff0fcfb87a761b7feca50481f68f284990
Henrique Ritter <henrique-ritter@hotmail.com>
2021-06-23 17:46:29 -0300

    Added Tag Service/Controller and post route

--- src/services/CreateTagService.ts
+++ src/services/CreateTagService.ts
@@ -1,19 +1,28 @@
 import { getCustomRepository } from 'typeorm';
 import { TagsRepositories } from '../repositories/TagsRepositories'
 
-interface IRequestTag {
-  name: string
-}
-
 class CreateTagService {
-  async execute({ name }: IRequestTag) {
+  async execute(name: string) {
+    if (!name) {
+      throw new Error('Incorrect name')
+    }
+
     const tagsRepository = getCustomRepository(TagsRepositories);
 
+    //verifica se existe tag com o mesmo nome
+    const tagAlreadyExists = await tagsRepository.findOne({ name })
+    if (tagAlreadyExists) {
+      throw new Error('Tag already exists')
+    }
+
+    //cria nova tag
     const tag = tagsRepository.create({
       name
     });
 
-    await tagsRepository.save(tag)
+    await tagsRepository.save(tag);
+
+    return tag;
   }
 }
 
