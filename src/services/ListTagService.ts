3ed641f8dfd6aeb6b3c8ddf559cd03cdebac15e1
Henrique Ritter <henrique-ritter@hotmail.com>
2021-06-25 19:54:47 -0300

    Add Class-Transformer / Tag Custom name

--- src/services/ListTagService.ts
+++ src/services/ListTagService.ts
@@ -1,17 +1,15 @@
 import { getCustomRepository } from "typeorm"
 import { TagsRepositories } from "../repositories/TagsRepositories"
+import { classToPlain } from 'class-transformer'
 
 class ListTagService {
   async execute() {
     const tagsRepository = getCustomRepository(TagsRepositories);
 
     const tags = await tagsRepository.find();
-    /*
-    let tags = await tagsRepository.find();
-    tags=tags.map((tag)=>({...tag,nameCustome: `#${tag.name}`}))
-    */
 
-    return tags;
+    //cria novos objetos a partir do que vem do TypeORM adicionando o objeto name_custom que criamos
+    return classToPlain(tags);
   }
 }
 
